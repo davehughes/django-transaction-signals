3214c21c2b884f40862f56acce02c65fed13a3e4
George Kappel <george.kappel@gmail.com>
2012-03-19 11:30:04 -0500

    Added missing comma

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import setuptools
 setuptools.setup(
-    name='django_transaction_signals'
+    name='django_transaction_signals',
     version='0.1.0',
     packages = setuptools.find_packages(),
 )
